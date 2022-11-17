2b5f39f1750d58295064fd6b6da203628722825b
Michael Bunte <michaelbunte@Michaels-MacBook-Pro-2.local>
2022-11-16 21:27:49 -0800

    boxes can now be given a state (wall, empty, or selected), and displayed

--- path_finder/sketch.js
+++ path_finder/sketch.js
@@ -1,9 +1,22 @@
 let boxCount;
 let boxSize;
+let boxes;
 
 function setup() {
   boxCount = createVector(20, 10);
   boxSize = 10;
+  boxes = [];
+  for (let i = 0; i < boxCount.y; i++) {
+    boxes.push([]);
+    for(let j = 0; j < boxCount.x; j++) {
+      let newBox = new Box(j, i, boxSize);
+      boxes[i].push(newBox);
+
+      if(random(0, 100) > 70) {
+        boxes[i][j].makeWall();
+      }
+    }
+  }
   createCanvas(boxCount.x * boxSize, boxCount.y * boxSize);
 }
 
@@ -12,7 +25,6 @@ class Box {
     this.index = createVector(x, y);
     this.pos = createVector(x * size, y * size);
     this.size = size;
-
     this.state = "empty";
   }
 
@@ -28,11 +40,34 @@ class Box {
     this.state = "selected";
   }
 
-  draw() {
-    rect(this.pos.x, this.pos.y, size, size);
+  display() {
+    switch (this.state) {
+      case "empty":
+        fill(255, 255, 255);
+        break;
+      case "wall":
+        fill(0, 0, 0);
+        break;
+      case "selected":
+        fill(0, 0, 255);
+        break;
+      default: 
+        fill(255, 0, 0);
+    }
+    strokeWeight(0);
+    rect(this.pos.x, this.pos.y, this.size, this.size);
+  }
+}
+
+function drawBoxes() {
+  for (let i = 0; i < boxCount.y; i++) {
+    for(let j = 0; j < boxCount.x; j++) {
+      boxes[i][j].display();
+    }
   }
 }
 
 function draw() {
   background(250);
+  drawBoxes();
 }
