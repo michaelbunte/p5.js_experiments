d3137c524b68f59abf983b3b703a11e9fab1f787
Michael Bunte <michaelbunte@Michaels-MacBook-Pro-2.local>
2022-11-16 21:02:14 -0800

    created a box class with states

--- path_finder/sketch.js
+++ path_finder/sketch.js
@@ -1,7 +1,38 @@
+let boxCount;
+let boxSize;
+
 function setup() {
-  createCanvas(400,400);
+  boxCount = createVector(20, 10);
+  boxSize = 10;
+  createCanvas(boxCount.x * boxSize, boxCount.y * boxSize);
+}
+
+class Box {
+  constructor(x, y, size) {
+    this.index = createVector(x, y);
+    this.pos = createVector(x * size, y * size);
+    this.size = size;
+
+    this.state = "empty";
+  }
+
+  makeWall() {
+    this.state = "wall";
+  }
+
+  makeEmpty() {
+    this.state = "empty";
+  }
+
+  makeSelected() {
+    this.state = "selected";
+  }
+
+  draw() {
+    rect(this.pos.x, this.pos.y, size, size);
+  }
 }
 
 function draw() {
-  background(220);
+  background(250);
 }
