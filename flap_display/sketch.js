d647e189edcb68a5bda5ab27e3d444eb4a237808
Michael Bunte <michaelbunte@Michaels-MacBook-Pro-2.local>
2022-11-19 16:34:32 -0800

    when letters are changed, they now cycle through available characters until settling on the inputted character

--- flap_display/sketch.js
+++ flap_display/sketch.js
@@ -6,13 +6,24 @@ let desiredLetters;
 let input;
 let canvasDims;
 let currLetters;
+let acceptedChars;
+
 function setup() {
-    dimensions = createVector(10, 5);
+
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_',.? ";
+    acceptedChars = [];
+    for(let i = 0; i < chars.length; i++) {
+        acceptedChars.push(chars[i]);
+    }
+
+    dimensions = createVector(15, 8);
     letterSize = createVector(16, 24);
     mainMargin = 20;
-    letterMargin = 5;
+    letterMargin = 2;
     desiredLetters = Array(dimensions.x * dimensions.y).fill(' ');
-    input = createInput('');
+    currLetters = [...desiredLetters];
+    // currLetters = desiredLetters;
+    input = createElement("textarea");
     input.input(myInputEvent);
     canvasDims = createVector(2 * mainMargin + letterSize.x * dimensions.x + letterMargin * (dimensions.x - 1), 
     2 * mainMargin + letterSize.y * dimensions.y + letterMargin * (dimensions.y - 1));    
@@ -31,18 +42,33 @@ function myInputEvent() {
     
 }
 
+function updateLetters() {
+    
+    for(let i = 0; i < currLetters.length; i++) {
+        if(desiredLetters[i] == currLetters[i]) {
+            continue;
+        }
+        let ind = acceptedChars.indexOf(currLetters[i]);
+        ind++;
+        if(ind >= acceptedChars.length) {
+            ind = 0;
+        }
+        currLetters[i] = acceptedChars[ind];
+    }
+}
+
 function drawLetters() {
     strokeWeight(0);
     let posX = mainMargin;
     let posY = mainMargin;
     let xLetters = 0; 
     textAlign(CENTER, CENTER);
+    textStyle(BOLD);
     for(let i = 0; i < desiredLetters.length; i++) {
-        fill(100);
+        fill(0);
         rect(posX, posY, letterSize.x, letterSize.y);
         fill(255);
-        text(desiredLetters[i], posX + letterSize.x/2, posY + letterSize.y/2);
-
+        text(currLetters[i], posX + letterSize.x/2, posY + letterSize.y/2);
         posX += letterMargin + letterSize.x;
         xLetters++;
         if(xLetters >= dimensions.x) {
@@ -54,6 +80,13 @@ function drawLetters() {
 }
 
 function draw() {
-    background(0);
+    background(60);
+
+    if(frameCount % 2 == 0) {
+        updateLetters();
+    }
+
+
     drawLetters();
+    console.log(desiredLetters);
 }
